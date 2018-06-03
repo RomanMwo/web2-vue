43c51dd9305ac27665a707a6d1a34c57719e9cc3
romanchlebus <orlik313@gmail.com>
2018-06-03 14:19:46 +0200

    after milligram

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id = "app">
+ <div id ="beforeLogin" v-if="!isAuthentication">
+  <h1> Witaj w systemie zapisu na zajecia !!!</h1>
+  <span>Zaloguj sie e-mailem: </span> <input type="email" v-model="email">
+  <button @click="toogle()">Wchodze</button>
+ </div >
+ <div id= "afterLogin" v-else>
+   <h1> Witaj w systemie zapisu na zajecia !!!</h1>
+   <h2>Witaj {{email}}</h2>
+   <button @click="toogle2()">Wyloguj</button>
+ </div>
+</div>
 </template>
 
 <script>
+import "milligram";
 export default {
-  name: 'app',
-  data () {
+name: 'app',
+  data() {
     return {
-      msg: 'Hello MWO'
+      email: '',
+      isAuthentication: false
     }
+  },
+  methods: {
+  toogle() {
+    this.isAuthentication = !this.isAuthentication;
+    
+  },
+  toogle2() {
+    this.isAuthentication = !this.isAuthentication;
+    this.email ="";
   }
+ }
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -39,22 +45,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 h1, h2 {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  color: red;
 }
 </style>
